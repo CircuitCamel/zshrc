6628851e8787c7a64fca5732e98785da7c26b319
CircuitCamel <cc@caml.cc>
2025-11-03 19:08:28 +0300

    added universal scripts dir

--- git.zsh
+++ git.zsh
@@ -1,5 +1,6 @@
-alias glazy="bash ~/.config/zshrc/scripts/glazy.sh"
-alias gtrack="bash ~/.config/zshrc/scripts/gtrack.sh"
+SCRIPT_DIR="${${(%):-%x}:A:h}/scripts"
+alias glazy="bash $SCRIPT_DIR/glazy.sh"
+alias gtrack="bash $SCRIPT_DIR/gtrack.sh"
 alias gp="git push"
 alias gu="git pull"
 alias gl="git log --oneline --graph"
@@ -8,3 +9,5 @@ alias gc="git commit -m"
 alias ga="git add"
 alias gstat="git status"
 alias gcl="git clone"
+
+unset SCRIPT_DIR
